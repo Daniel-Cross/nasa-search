3c6539f0ea49d6dad4d5d55036272e653878c92e
Joe Stephens <joe@webantic.co.uk>
2018-07-11 02:52:47 +0100

    Add limit to passed props

--- src/providers/withPagination.js
+++ src/providers/withPagination.js
@@ -8,25 +8,33 @@ import PropTypes from 'prop-types';
 class PaginationWrapper extends Component {
   state = {
     currentPage: 1,
-    limit: 20,
+    limit: 10,
   };
 
   setPage = currentPage => this.setState({ currentPage });
 
   render() {
-    const { currentPage } = this.state;
+    const { currentPage, limit } = this.state;
     const { component: Child, ...props } = this.props;
 
     return (
       <Fragment>
-        <Child currentPage={currentPage} setPage={this.setPage} {...props} />
+        <Child
+          limit={limit}
+          currentPage={currentPage}
+          setPage={this.setPage}
+          {...props}
+        />
       </Fragment>
     );
   }
 }
 
 PaginationWrapper.propTypes = {
-  component: PropTypes.oneOf([PropTypes.element, PropTypes.func]).isRequired,
+  component: PropTypes.oneOfType([
+    PropTypes.instanceOf(Component),
+    PropTypes.func,
+  ]).isRequired,
 };
 
 export const withPagination = component => props => ( // eslint-disable-line
